13deee299b844df05b83023cf83208d0a0196132
Charlie Vuong <charlie@upthere.com>
2016-02-21 10:30:37 -0800

    Add mkdirp to create the public folder

--- scripts/make_static_pages.js
+++ scripts/make_static_pages.js
@@ -3,23 +3,19 @@ var ejs = require('ejs'),
     path = require('path'),
     mkdirp = require('mkdirp'),
     ejsPath = path.resolve(__dirname, '../src', 'ejs'),
-    ejsFilenames;
+    ejsFilenames, publicPath;
 
-console.log('ejs path', ejsPath);
+publicPath = path.resolve(__dirname, '../public');
 
 // For each ejs file, compile the html version and output to the public
 // directory.
 ejsFilenames = fs.readdirSync(ejsPath);
-console.log('ejs filenames', ejsFilenames);
+mkdirp.sync(publicPath);
 ejsFilenames.forEach(function(filename) {
   var contents = fs.readFileSync(path.resolve(ejsPath, filename), 'utf8');
   var html = ejs.render(contents);
   var filenameAsHtml = path.basename(filename, '.ejs') + '.html';
-  console.log('filename as html', filenameAsHtml);
-  fs.writeFileSync(path.resolve(__dirname, '..', '/public', filenameAsHtml), html);
+  fs.writeFileSync(path.resolve(publicPath, filenameAsHtml), html);
 });
-
-
-
 
 
